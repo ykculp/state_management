9b2df6860a7c39cb5553f4b9a92dc0a2123f9bc9
ykculp <83663421+ykculp@users.noreply.github.com>
2021-12-09 17:48:29 -0500

    Initialize alternative

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:misiontic_template/presentation/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
